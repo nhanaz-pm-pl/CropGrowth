a6aff0d8f1c94b400213dff3e37a55e6d58b80b4
Thành Nhân <nhangaming125@gmail.com>
2021-12-06 07:16:09 +0700

    Fix Poggit Lint!

--- src/Main.php
+++ src/Main.php
@@ -29,53 +29,53 @@ class Main extends PluginBase implements Listener
 					do {
 						$z1 = mt_rand(5, 20) / 10;
 						$y1 = mt_rand(11, 21) / 10;
-						$coordinates_particle = $block->add(0.5, $y1, $z1);
+						$coordinates_particle = $block->getPosition()->add(0.5, $y1, $z1);
 						$summon_particle = new HappyVillagerParticle($coordinates_particle);
-						$block->getWorld()->addParticle($summon_particle);
+						$block->addParticle($summon_particle);
 
 						$x1 = mt_rand(-20, 5) / 10;
 						$z2 = mt_rand(5, 20) / 10;
 						$y2 = mt_rand(11, 21) / 10;
-						$coordinates_particle = $block->add($x1, $y2, $z2);
+						$coordinates_particle = $block->getPosition()->add($x1, $y2, $z2);
 						$summon_particle = new HappyVillagerParticle($coordinates_particle);
 						$block->getWorld()->addParticle($summon_particle);
 
 						$x3 = mt_rand(-20, 5) / 10;
 						$y3 = mt_rand(11, 21) / 10;
-						$coordinates_particle = $block->add($x3, $y3, 0.5);
+						$coordinates_particle = $block->getPosition()->add($x3, $y3, 0.5);
 						$summon_particle = new HappyVillagerParticle($coordinates_particle);
 						$block->getWorld()->addParticle($summon_particle);
 
 						$x4 = mt_rand(-20, 5) / 10;
 						$z3 = mt_rand(-20, 5) / 10;
 						$y4 = mt_rand(11, 21) / 10;
-						$coordinates_particle = $block->add($x4, $y4, $z3);
+						$coordinates_particle = $block->getPosition()->add($x4, $y4, $z3);
 						$summon_particle = new HappyVillagerParticle($coordinates_particle);
 						$block->getWorld()->addParticle($summon_particle);
 
 						$z4 = mt_rand(-20, 5) / 10;
 						$y5 = mt_rand(11, 21) / 10;
-						$coordinates_particle = $block->add(0.5, $y5, $z4);
+						$coordinates_particle = $block->getPosition()->add(0.5, $y5, $z4);
 						$summon_particle = new HappyVillagerParticle($coordinates_particle);
 						$block->getWorld()->addParticle($summon_particle);
 
 						$x5 = mt_rand(5, 20) / 10;
 						$z5 = mt_rand(-20, 5) / 10;
 						$y6 = mt_rand(11, 21) / 10;
-						$coordinates_particle = $block->add($x5, $y6, $z5);
+						$coordinates_particle = $block->getPosition()->add($x5, $y6, $z5);
 						$summon_particle = new HappyVillagerParticle($coordinates_particle);
 						$block->getWorld()->addParticle($summon_particle);
 
 						$x6 = mt_rand(5, 20) / 10;
 						$y7 = mt_rand(11, 21) / 10;
-						$coordinates_particle = $block->add($x6, $y7, 0.5);
+						$coordinates_particle = $block->getPosition()->add($x6, $y7, 0.5);
 						$summon_particle = new HappyVillagerParticle($coordinates_particle);
 						$block->getWorld()->addParticle($summon_particle);
 
 						$x7 = mt_rand(5, 20) / 10;
 						$z6 = mt_rand(5, 20) / 10;
 						$y8 = mt_rand(11, 21) / 10;
-						$coordinates_particle = $block->add($x7, $y8, $z6);
+						$coordinates_particle = $block->getPosition()->add($x7, $y8, $z6);
 						$summon_particle = new HappyVillagerParticle($coordinates_particle);
 						$block->getWorld()->addParticle($summon_particle);
 					/** @phpstan-ignore-next-line */
