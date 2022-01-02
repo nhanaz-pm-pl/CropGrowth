ce4a91e6268a72137829b6559a94319bfb18b882
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-01-02 13:01:45 +0700

    Performance Optimization
    - Code cleanup
    - Remove comments
    - Clean description
    - Change icon
    
    Co-Authored-By: Kaido Joestar <45457914+kaidoMC@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # General
-**A plugin that makes it possible to see green particles when using bone meal on grass blocks**
+**Show particles when using bone meal on grass block**
 
 # Contacts
-**You can contact me directly through the platforms listed below**
-| Platform | Contact            |
-| -------- | ------------------ |
-| Discord  | NhanAZ#9115        |
-| Email    | NhanAZ@pm.me       |
+**You can contact me directly via Discord NhanAZ#9115**
--- src/Main.php
+++ src/Main.php
@@ -5,89 +5,30 @@ declare(strict_types=1);
 namespace NhanAZ\CropGrowth;
 
 use pocketmine\block\Grass;
-use pocketmine\math\Vector3;
 use pocketmine\event\Listener;
 use pocketmine\item\Fertilizer;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\world\particle\HappyVillagerParticle;
+use pocketmine\network\mcpe\protocol\SpawnParticleEffectPacket;
 
-/**
- * Class Main
- * @package NhanAZ\CropGrowth
- */
-class Main extends PluginBase implements Listener
-{
+class Main extends PluginBase implements Listener {
 
-	/**
-	 * @return void
-	 */
-	protected function onEnable(): void
-	{
+	protected function onEnable() : void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
-	/**
-	 * @param PlayerInteractEvent $event
-	 *
-	 * @priority HIGHEST
-	 */
-	public function onPlayerInteract(PlayerInteractEvent $event)
-	{
-		$block = $event->getBlock();
-		$particle = new HappyVillagerParticle();
-		$getBlockPosition = $block->getPosition();
-		$getWorld = $getBlockPosition->getWorld();
-		$rightClickBlock = PlayerInteractEvent::RIGHT_CLICK_BLOCK;
-		if ($block instanceof Grass) {
-			if ($event->getItem() instanceof Fertilizer) {
+	public function onPlayerInteract(PlayerInteractEvent $event) {
+		if ($event->getItem() instanceof Fertilizer) {
+			$block = $event->getBlock();
+			if ($block instanceof Grass) {
+				$rightClickBlock = PlayerInteractEvent::RIGHT_CLICK_BLOCK;
 				if ($event->getAction() === $rightClickBlock) {
-					do {
-						$z1 = mt_rand(5, 20) / 10;
-						$y1 = mt_rand(11, 21) / 10;
-						$vector = $getBlockPosition->add(0.5, $y1, $z1);
-						$getWorld->addParticle($vector, $particle);
-
-						$x1 = mt_rand(-20, 5) / 10;
-						$z2 = mt_rand(5, 20) / 10;
-						$y2 = mt_rand(11, 21) / 10;
-						$vector = $getBlockPosition->add($x1, $y2, $z2);
-						$getWorld->addParticle($vector, $particle);
-
-						$x3 = mt_rand(-20, 5) / 10;
-						$y3 = mt_rand(11, 21) / 10;
-						$vector = $getBlockPosition->add($x3, $y3, 0.5);
-						$getWorld->addParticle($vector, $particle);
-
-						$x4 = mt_rand(-20, 5) / 10;
-						$z3 = mt_rand(-20, 5) / 10;
-						$y4 = mt_rand(11, 21) / 10;
-						$vector = $getBlockPosition->add($x4, $y4, $z3);
-						$getWorld->addParticle($vector, $particle);
-
-						$z4 = mt_rand(-20, 5) / 10;
-						$y5 = mt_rand(11, 21) / 10;
-						$vector = $getBlockPosition->add(0.5, $y5, $z4);
-						$getWorld->addParticle($vector, $particle);
-
-						$x5 = mt_rand(5, 20) / 10;
-						$z5 = mt_rand(-20, 5) / 10;
-						$y6 = mt_rand(11, 21) / 10;
-						$vector = $getBlockPosition->add($x5, $y6, $z5);
-						$getWorld->addParticle($vector, $particle);
-
-						$x6 = mt_rand(5, 20) / 10;
-						$y7 = mt_rand(11, 21) / 10;
-						$vector = $getBlockPosition->add($x6, $y7, 0.5);
-						$getWorld->addParticle($vector, $particle);
-
-						$x7 = mt_rand(5, 20) / 10;
-						$z6 = mt_rand(5, 20) / 10;
-						$y8 = mt_rand(11, 21) / 10;
-						$vector = $getBlockPosition->add($x7, $y8, $z6);
-						$getWorld->addParticle($vector, $particle);
-						/** @phpstan-ignore-next-line */
-					} while (false);
+					$position = $block->getPosition();
+					$packet = new SpawnParticleEffectPacket();
+					$packet->position = $position->add(0.5, 1.5, 0.5);
+					$packet->particleName = "minecraft:crop_growth_area_emitter";
+					$recipients = $this->getServer()->getOnlinePlayers();
+					$this->getServer()->broadcastPackets($recipients, [$packet]);
 				}
 			}
 		}
