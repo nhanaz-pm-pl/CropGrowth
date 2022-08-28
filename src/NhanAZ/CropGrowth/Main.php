454b9d6491f27527e27a2a204345ccbb11b9ae94
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-08-28 11:17:55 +0700

    Require usage of ::create() for packets

--- src/NhanAZ/CropGrowth/Main.php
+++ src/NhanAZ/CropGrowth/Main.php
@@ -4,31 +4,30 @@ declare(strict_types=1);
 
 namespace NhanAZ\CropGrowth;
 
+use pocketmine\block\VanillaBlocks;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\VanillaItems;
 use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
-use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
-use pocketmine\item\VanillaItems;
-use pocketmine\block\VanillaBlocks;
-use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\network\mcpe\protocol\SpawnParticleEffectPacket;
-use NhanAZ\CropGrowth\Crops;
+use pocketmine\network\mcpe\protocol\types\DimensionIds;
+use pocketmine\plugin\PluginBase;
+use function in_array;
 
 class Main extends PluginBase implements Listener {
-
 	protected function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
 	private function spawnParticleEffect(Vector3 $position, bool $particleArea): void {
-		$packet = new SpawnParticleEffectPacket();
-		$packet->position = $position;
 		if ($particleArea) {
-			$packet->particleName = "minecraft:crop_growth_area_emitter";
+			$particleName = 'minecraft:crop_growth_area_emitter';
 		} else {
-			$packet->position = $position->add(0.5, 0.5, 0.5);
-			$packet->particleName = "minecraft:crop_growth_emitter";
+			$position = $position->add(0.5, 0.5, 0.5);
+			$particleName = 'minecraft:crop_growth_emitter';
 		}
+		$packet = SpawnParticleEffectPacket::create(DimensionIds::OVERWORLD, -1, $position, $particleName, null);
 		$recipients = $this->getServer()->getOnlinePlayers();
 		$this->getServer()->broadcastPackets($recipients, [$packet]);
 	}
@@ -36,10 +35,9 @@ class Main extends PluginBase implements Listener {
 	public function onPlayerInteract(PlayerInteractEvent $event): void {
 		$block = $event->getBlock();
 		if ($event->getItem()->equals(VanillaItems::BONE_MEAL(), true)) {
-			if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
-
-				# Crop Ids With Area Emitter
-				if (in_array($block->getTypeId(), Crops::CropIdsWithAreaEmitter())) {
+			if (PlayerInteractEvent::RIGHT_CLICK_BLOCK === $event->getAction()) {
+				// Crop Ids With Area Emitter
+				if (in_array($block->getTypeId(), Crops::CropIdsWithAreaEmitter(), true)) {
 					if ($block->isSameType(VanillaBlocks::GRASS())) {
 						if ($block->getSide(Facing::UP)->isSameType(VanillaBlocks::AIR())) {
 							$position = $block->getPosition()->add(0.5, 1.5, 0.5);
@@ -51,8 +49,8 @@ class Main extends PluginBase implements Listener {
 					}
 				}
 
-				# Crop Ids With Emitter
-				if (in_array($block->getTypeId(), Crops::CropIdsWithEmitter())) {
+				// Crop Ids With Emitter
+				if (in_array($block->getTypeId(), Crops::CropIdsWithEmitter(), true)) {
 					$this->spawnParticleEffect($block->getPosition(), false);
 				}
 			}
